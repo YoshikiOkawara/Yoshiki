905fadf557827af6d495c88f2c2d1f0b82074906
EC2 Default User <ec2-user@ip-172-31-18-193.ec2.internal>
2024-06-21 17:25:29 +0900

    コミットメッセージ

---
+++ .c9/HelloWorld.php
@@ -0,0 +1,6 @@
+<?php
+
+echo 'HelloWorld!'.PHP_EOL;
+
+php /home/ec2-user/environment/HelloWorld.php
+?>
---
+++ .c9/getType.php
@@ -0,0 +1,14 @@
+<?php
+$str = "文字列です。"; 
+$int_number = 4;
+$arr = array(1,2,3,4); 
+$null = null;
+$true = true; 
+
+echo gettype($str) . PHP_EOL;
+echo gettype($int_number) . PHP_EOL;
+echo gettype($arr) . PHP_EOL;
+echo gettype($null) . PHP_EOL;
+echo gettype($true) . PHP_EOL;
+?>
+
